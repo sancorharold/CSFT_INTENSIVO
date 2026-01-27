14c81b1ceeb4698ae734d7b60b2c74448e67bcb0
JeanP88 <locomrbream88@gmail.com>
2026-01-26 21:25:52 -0500

    xxx

--- core/views.py
+++ core/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth.views import LoginView
+from django.urls import reverse_lazy
+from .mixins import TitleContextMixin
 
-# Create your views here.
+class CustomLoginView(TitleContextMixin, LoginView):
+    template_name = "login.html"
+    title2 = "Iniciar Sesión"
+
+    def get_success_url(self):
+        # Redirige a la página principal después de un login exitoso
+        return reverse_lazy("")
+
+
